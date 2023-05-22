74feb70e3f3dab61ee1537da6ed1a8b39aff92a8
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-22 17:55:35 +0900

    Library Service 인터페이스, 클래스 구현

---
+++ src/main/java/exam/springboot/jpa/library/service/LibraryService.java
@@ -0,0 +1,10 @@
+package exam.springboot.jpa.library.service;
+
+import exam.springboot.jpa.library.model.Library;
+
+import java.util.List;
+
+public interface LibraryService {
+
+    List<Library> readLibrary(int cpg);
+}
