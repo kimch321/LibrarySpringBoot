ca77b2642a35995060a2ee3a7618b2e38eeba818
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-22 17:10:59 +0900

    도서관 정보 리스트 페이지

--- src/main/java/exam/springboot/jpa/library/controller/LibraryController.java
+++ src/main/java/exam/springboot/jpa/library/controller/LibraryController.java
@@ -9,16 +9,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class LibraryController {
 
 
-    @GetMapping("/list")
-    public ModelAndView list(@RequestParam int cpg) {
+    @GetMapping("/")
+    public String list() {
         ModelAndView mv = new ModelAndView();
 
-        /*mv.addObject("lblist", lbsrv.readList(cpg));
-        mv.addObject("cpg",cpg);
-        mv.addObject("stpg", ((cpg-1)/10)*10 + 1);*/
-        /*mv.addObject("cntpg",lbsrv.countList());*/
+        /*mv.addObject("lblist", lbsrv.readList(cpg));*/
+        /*mv.addObject("cpg",cpg);
+        mv.addObject("stpg", ((cpg-1)/10)*10 + 1);
+        mv.addObject("cntpg",lbsrv.countList());*/
 
         mv.setViewName("list");
-        return mv;
+        return "list";
     }
 }
